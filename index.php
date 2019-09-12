da9f1ac029d0b6e9e700c09571c15e106567dac6
Jared Bean <jaredbean@mail.weber.edu>
2019-09-11 20:32:41 -0600

    index.php and login.php files setup.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Keyboard Typing Game</title>
+    <style></style>
+  </head>
+  <body>
+    <h1>Keyboard Typing Game</h1>
+    <nav>
+      <ul>
+	<li><a href="login.php">Login Page</a></li>
+      </ul>
+    </nav>
+      
+
+  </body>
+</html>
+
+    
