68e13008d5285b74d8dd32b26d475b9e4e5caacd
Jared Bean <jaredbean@mail.weber.edu>
2019-09-16 21:09:39 -0600

    Form and post method functioning.

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>Keyboard Typing Game</title>
-    <style></style>
-  </head>
-  <body>
-    <h1>Keyboard Typing Game</h1>
-    <nav>
-      <ul>
-	<li><a href="login.php">Login Page</a></li>
-      </ul>
-    </nav>
-      
+<head>
+  <meta charset="UTF-8">
+  <title>Keyboard Typing Game</title>
+  <style></style>
+</head>
+<body>
 
-  </body>
+<h1>Keyboard Typing Game</h1>
+
+<form action="" method="post">
+  <label for="username">Username:</label>
+  <input type="text" name="username" id="username"><br>
+  <label for="password">Password:</label>
+  <input type="text" name="password" id="password"><br>
+  <input type="submit" value="Submit">
+</form>
+
+<?php echo $_POST['username']; ?>
+
+</body>
 </html>
 
     
