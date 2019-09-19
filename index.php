10c014a58a1ae364942dcf5030a48d7a4531335a
Jared Bean <jared.d.bean@gmail.com>
2019-09-18 18:32:16 -0600

    Added login.js file which hashes a password with salt, using SHA-256.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <title>Keyboard Typing Game</title>
   <style></style>
+  <script type="text/javascript" src="login.js"></script>
 </head>
 <body>
 
@@ -14,10 +15,13 @@
   <input type="text" name="username" id="username"><br>
   <label for="password">Password:</label>
   <input type="text" name="password" id="password"><br>
-  <input type="submit" value="Submit">
+  <!-- <input type="submit" value="Submit"> -->
+  <input type="button" value="Sign Up!" id="btnSignUp" name="btnSignUp" onclick="createUser()">
 </form>
 
 <?php echo $_POST['username']; ?>
+
+<script>generateSalt()</script>
 
 </body>
 </html>
